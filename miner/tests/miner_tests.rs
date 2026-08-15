62d2493440d795702c35e8b01968e26350b7f20e
Aditaya08 <aditayash625@gmail.com>
2026-08-15 15:25:58 +0530

    test(miner): cover template mining

---
+++ miner/tests/miner_tests.rs
@@ -0,0 +1,24 @@
+use btclib::crypto::PrivateKey;
+use btclib::sha256::Hash;
+use btclib::types::{Block, BlockHeader, Transaction, TransactionOutput};
+use btclib::util::MerkleRoot;
+use chrono::Utc;
+use miner::mine_template;
+
+#[test]
+fn mine_template_returns_a_block_that_satisfies_target() {
+    let key = PrivateKey::new_key();
+    let transactions = vec![Transaction::coinbase(TransactionOutput::new(50, key.public_key()))];
+    let block = Block::new(
+        BlockHeader::new(
+            Utc::now(),
+            0,
+            Hash::zero(),
+            MerkleRoot::calculate(&transactions),
+            btclib::min_target(),
+        ),
+        transactions,
+    );
+    let mined = mine_template(block, 2_000_000).expect("block should mine with easy target");
+    assert!(mined.hash().matches_target(mined.header.target));
+}
